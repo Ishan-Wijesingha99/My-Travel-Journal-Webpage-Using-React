46febe23b2e13fe5f0cc12500af266c0320a8115
Ishan <you@example.com>
2022-06-19 00:12:24 +1000

    finished project completely

--- src/components/singleCard.js
+++ src/components/singleCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../index.css'
+import mapMarker from '../images/mapMarker.png'
 
 
 export const SingleCardRender = function(props) {
@@ -7,9 +8,15 @@ export const SingleCardRender = function(props) {
         <div className='card'>
             <img id='card-image' src={props.img} alt='card-image'/>
 
-            <p id='card-location'>{props.location}</p>
-            <a id='google-maps-link'>View on Google Maps</a>
             <p id='card-title'>{props.title}</p>
+
+
+            <div className='marker-and-location'>
+                <img id='map-marker' src={mapMarker} alt='map marker image'/>
+                <p id='card-location'>{props.location}</p>
+            </div>
+
+            <a id='google-maps-link' href={props.googleMapsUrl}>View on Google Maps</a>
             <p id='card-dates'>{props.startDate} - {props.endDate}</p>
             <p id='card-description'>{props.description}</p>
         </div>
