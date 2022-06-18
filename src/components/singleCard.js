75735ecc1107bb70bef9d7e8ca80bdb63030d23e
Ishan <you@example.com>
2022-06-18 22:37:58 +1000

    finished grid layout of grid elements

--- src/components/singleCard.js
+++ src/components/singleCard.js
@@ -7,10 +7,11 @@ export const SingleCardRender = function(props) {
         <div className='card'>
             <img id='card-image' src={props.img} alt='card-image'/>
 
-            <p>{props.location}</p>
-            <p>{props.title}</p>
-            <p>{props.startDate} - {props.endDate}</p>
-            <p>{props.description}</p>
+            <p id='card-location'>{props.location}</p>
+            <a id='google-maps-link'>View on Google Maps</a>
+            <p id='card-title'>{props.title}</p>
+            <p id='card-dates'>{props.startDate} - {props.endDate}</p>
+            <p id='card-description'>{props.description}</p>
         </div>
     )
 }
